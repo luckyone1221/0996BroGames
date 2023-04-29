5c1b4f6553057f355e351be33b3471426d6be2f6
Serhiy Fedorchuck <serhiy.fedorchuck@mediamonks.com>
2023-04-30 01:14:04 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {MainPage} from "./blocks/MainPage/MainPage";
+import './sass/main.scss';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<MainPage/>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/blocks/MainPage/MainPage.js
@@ -0,0 +1,10 @@
+import {Header} from "../Header/Header";
+
+export const MainPage = (props) => {
+  return(
+    <div className="main-wrapper">
+      <Header/>
+      lalala
+    </div>
+  )
+}
